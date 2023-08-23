c4eae2553f6f2d1f2b0d422a76a364062e771eb2
ayoubsaidi906 <ayoubsaidi906@gmail.com>
2023-08-23 12:52:21 -0700

    hot fix

--- a_printf.c
+++ a_printf.c
@@ -14,28 +14,26 @@ int _printf(const char *format, ...)
 
 	va_start(arg, format);
 
-	if (!format || (format[0] == '%' && !format[1]))
-	{
-		return (-1);
-	}
-	if (format[0] == '%' && format[1] == ' ' && !format[2])
-	{
-		return (-1);
-	}
-	while (*format)
-	{
-	if (*format == '%')
-	{
-		format++;
-		handle_for(arg, *format, &i);
-	}
-		else
-		{
+if (!format || (format[0] == '%' && (!format[1] || format[1] == ' ')))
+{
+return (-1);
+}
+
+while (*format)
+{
+if (*format == '%')
+{
+format++;
+	handle_for(arg, *format, &i);
+}
+else
+{
 _putchar(*format);
 i++;
-		}
-		format++;
-	}
+}
+format++;
+}
+
 	va_end(arg);
 	return (i);
 }
