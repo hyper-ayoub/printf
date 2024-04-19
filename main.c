575f07a2c85e399918eef124bebc431bde8b0632
ayoub bouagna <133155846+hyper-ayoub@users.noreply.github.com>
2024-04-19 23:22:35 +0100

    Update main.c

--- main.c
+++ main.c
@@ -13,7 +13,10 @@ int main(void)
     int len2;
     unsigned int ui;
     void *addr;
-
+    len = _printf("%.6s", "Best School !\n");
+	len2 = printf("%.6s", "Best School !\n");
+    len = _printf("%.*s", 6, "Best School !\n");
+	len2 = printf("%.*s", 6, "Best School !\n");
     len = _printf("Let's try to printf a simple sentence.\n");
     len2 = printf("Let's try to printf a simple sentence.\n");
     ui = (unsigned int)INT_MAX + 1024;
